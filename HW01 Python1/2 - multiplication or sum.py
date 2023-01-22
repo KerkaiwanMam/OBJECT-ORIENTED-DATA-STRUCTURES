ac333b944ad5d9ce0a648f3c86cc71848165d33f
KerkaiwanMam <a370957083131@gmail.com>
2023-01-22 23:56:34 +0700

    update item1&2

--- HW01 Python1/2 - multiplication or sum.py
+++ HW01 Python1/2 - multiplication or sum.py
@@ -1,3 +1,20 @@
+'''
+รับ input จำนวนเต็มสองจำนวน หากผลคูณของทั้งสองจำนวนมีค่าเกิน 1000 ให้ show 
+ผลรวมของจำนวนทั้งสอง แต่หากผลคูณมีค่าน้อยกว่าหรือเท่ากับ 1,000 
+ให้ show ผลคูณของจำนวนทั้งสอง
+
+
+*** multiplication or sum ***
+Enter num1 num2 : 20 30
+The result is 600
+
+
+*** multiplication or sum ***
+Enter num1 num2 : 40 60
+The result is 100
+
+'''
+
 print("*** multiplication or sum ***")
 num1,num2 = map(int,input("Enter num1 num2 : ").split())
 
