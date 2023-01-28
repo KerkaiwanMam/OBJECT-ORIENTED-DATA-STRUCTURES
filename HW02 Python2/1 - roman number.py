78ecaf2b694a9ef31c1bf99b98814c93990ff7a9
KerkaiwanMam <a370957083131@gmail.com>
2023-01-28 12:54:18 +0700

    HW2

---
+++ HW02 Python2/1 - roman number.py
@@ -0,0 +1,183 @@
+'''
+จงเขียนฟังชั่นแปลง เลขอารบิกเป็นเลขโรมัน และ เลขโรมันเป็นอารบิกโดยที่
+
+M=1000    CM=900    D=500    CD=400,
+
+C=100    XC=90    L=50    XL=40,
+
+X=10    IX=9    V=5    IV=4    I=1
+
+เช่น 197 = 100 + 90 +7 = 100 + 90 + 5 + 1 + 1 = C XC V I I
+
+(https://roman-numerals.info/)
+
+
+
+class translator:
+
+    def deciToRoman(self, num):
+
+        ### Enter Your Code Here ###
+
+    def romanToDeci(self, s):
+
+        ### Enter Your Code Here ###
+
+num = int(input("Enter number to translate : "))
+
+print(translator().deciToRoman(num))
+
+print(translator().romanToDeci(translator().deciToRoman(num)))
+
+
+Enter number to translate : 5
+V
+5
+
+
+Enter number to translate : 256
+CCLVI
+256
+
+Enter number to translate : 555
+DLV
+555
+
+'''
+
+class translator:
+
+    def deciToRoman(self, num):
+        s=''
+        if num // 1000 >= 1:
+            for i in range(0,num //1000):
+                s += 'M'
+                num -=1000
+
+        if num//900 >=1:
+            for i in range(0,num//900):
+                s+='CM'
+                num -= 900
+        
+        if num//500 >= 1:
+            for i in range(0,num//500):
+                s +='D'
+                num -= 500
+        
+        if num//400 >=1:
+            for i in range(0,num//400):
+                s +='CD'
+                num -= 400
+        
+        if num//100 >= 1:
+            for i in range(0,num//100):
+                s += 'C'
+                num -= 100
+        
+        if num//90 >= 1:
+            for i in range(0,num//90):
+                s += 'XC'
+                num -= 90
+
+        if num//50 >= 1:
+            for i in range(0,num//50):
+                s += 'L'
+                num -= 50
+        
+        if num//40 >= 1:
+            for i in range(0,num//40):
+                s += 'XL'
+                num -= 40
+        
+        if num//10 >= 1:
+            for i in range(0,num//10):
+                s += 'X'
+                num -= 10
+        
+        if num//9 >= 1:
+            for i in range(0,num//9):
+                s += 'IX'
+                num -= 9
+        
+        if num//5 >= 1:
+            for i in range(0,num//5):
+                s += 'V'
+                num -= 5
+
+        if num//4 >= 1:
+            for i in range(0,num//4):
+                s += 'IV'
+                num -= 4
+
+        if num//1 >= 1:
+            for i in range(0,num//1):
+                s += 'I'
+                num -= 1
+        return s
+        
+         
+    def romanToDeci(self, s):
+        num = 0
+        s = list(s)
+        while len(s) > 0:
+            if s[-1] == 'I':
+                num += 1
+                s.pop()
+            elif s[-1] == 'V':
+                if len(s) > 1 and  s[-2] =='I':
+                    num += 4
+                    s.pop()
+                    s.pop()
+                else :
+                    num +=5
+                    s.pop()
+            elif s[-1] == 'X' :
+                if len(s) > 1 and s[-2] == 'I':
+                    num += 9
+                    s.pop()
+                    s.pop()
+                else:
+                    num += 10
+                    s.pop()
+            
+            elif s[-1] == 'L':
+                if len(s) > 1 and s[-2] == 'X' :
+                    num += 40
+                    s.pop()
+                    s.pop()
+                else :
+                    num += 50
+                    s.pop()
+            
+            elif s[-1] == 'C' :
+                if len(s) > 1 and s [-2] == 'X' :
+                    num += 90
+                    s.pop()
+                    s.pop()
+                else :
+                    num += 100
+                    s.pop()
+            elif s[-1] == 'D' :
+                if len(s) > 1 and s[-2] == 'C' :
+                    num += 400
+                    s.pop()
+                    s.pop()
+                else :
+                    num += 500
+                    s.pop()
+            elif s[-1] == 'M' :
+                if len(s) > 1 and s[-2] == 'C' :
+                    num += 900 
+                    s.pop()
+                    s.pop()
+                else :
+                    num += 1000
+                    s.pop()
+        return num
+
+num = int(input("Enter number to translate : "))
+
+print(translator().deciToRoman(num))
+
+print(translator().romanToDeci(translator().deciToRoman(num)))
+
